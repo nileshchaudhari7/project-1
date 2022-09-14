ab08097b1a87df8d6470cd9b406e3b23c6afd05d
Nilesh Chaudhari <95333574+G12H782@users.noreply.github.com>
2022-09-14 14:46:51 +0530

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,16 @@
+# List [] collection which is ordered and changeable : allows duplicate
+# Tuple () collection which is ordered and unchangeable : allows duplicate
+# Set {"Banana"} collection which is unordered, unchangeable and unindexed : not allows duplicate
+# Dict {Fruits : Banana} collection which is unordered, changeable and indexed : not allows duplicate
+"""
+fruits = {"Name : John" , "Id : 998" , "Dept : Automation"}
+#fruits[0] = ("Name : MAN")
+for u in fruits:
+    print(u)
+"""
+
+def mod(x,y):
+    return x % y
+
+
+mod(5,2)
---
+++ functions.py
@@ -0,0 +1,21 @@
+def hui(x,y):
+    return x * y
+
+
+print(hui(4,2))
+
+
+# functions
+def mod(x,y):
+    return x % y
+
+
+print(mod(4,2))
+
+def greet(name="default name"):
+	    print("Hello " + name)
+
+
+greet("Mosh")
+
+
