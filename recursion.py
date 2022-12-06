7c8aa8fe025a43bc68c621bc333424db9749c4c8
Nilesh Chaudhari <nilesh.chaudhari@man-es.com>
2022-12-06 12:23:48 +0530

    add recursion code in python

---
+++ recursion.py
@@ -0,0 +1,13 @@
+List = ['a','b','c','d']
+List.append("e")
+List = ['a','b','c',['e','d',['target']]]
+Sort = List[3][2][0][4]
+print(Sort)
+Mod = 5%2
+print(Mod)
+Number = float(input("Put Number "))
+Mod_1 = Number%2
+if Mod_1 == 0:
+    print("Even")
+else:
+    print("Odd")
